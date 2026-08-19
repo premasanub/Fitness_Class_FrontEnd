ed397091263335bb4806cadb5f6ffe9348e25f04
premasanub <premakumaran15@gmail.com>
2026-08-19 11:27:30 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ import { FaDumbbell } from "react-icons/fa";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 function Navbar() {
 
   const { user, logout } = useAuth();
@@ -24,8 +25,16 @@ function Navbar() {
 
       <ul className="hidden md:flex gap-8">
         <Link to="/"> <li className="cursor-pointer hover:text-blue-500">Home</li> </Link>
+
+
+        <NavLink to="/dashboard/classes">
+  <li className="flex items-center gap-3 cursor-pointer hover:text-blue-400">
+    <FaDumbbell />
+    Classes
+  </li>
+</NavLink>
         
-      <Link to="/dashboard/classes"> <li className="cursor-pointer hover:text-blue-500">Classes</li> </Link>
+      {/* <Link to="/dashboard/classes"> <li className="cursor-pointer hover:text-blue-500">Classes</li> </Link> */}
 
        <Link to="/trainers"> <li className="cursor-pointer hover:text-blue-500">Trainers</li></Link>
         <li className="cursor-pointer hover:text-blue-500">About</li>
