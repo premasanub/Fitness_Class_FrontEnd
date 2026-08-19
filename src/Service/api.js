feb2b7bf4a0f94a0ad0e87bdd8eb94a39a99b597
premasanub <premakumaran15@gmail.com>
2026-08-19 09:42:09 +0530

    upadated

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://fitness-class-backend.onrender.com",
+  baseURL: "https://fitness-class-backend.onrender.com/api",
 });
 
 export default api;
