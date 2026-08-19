df785e8f2cc03c37560e195c72bbda0206e45a54
premasanub <premakumaran15@gmail.com>
2026-08-19 10:31:36 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -24,7 +24,8 @@ function Navbar() {
 
       <ul className="hidden md:flex gap-8">
         <Link to="/"> <li className="cursor-pointer hover:text-blue-500">Home</li> </Link>
-        <li className="cursor-pointer hover:text-blue-500">Classes</li>
+
+      <Link to="/classes">  <li className="cursor-pointer hover:text-blue-500">Classes</li></Link>
        <Link to="/trainers"> <li className="cursor-pointer hover:text-blue-500">Trainers</li></Link>
         <li className="cursor-pointer hover:text-blue-500">About</li>
         <li className="cursor-pointer hover:text-blue-500">Contact</li>
