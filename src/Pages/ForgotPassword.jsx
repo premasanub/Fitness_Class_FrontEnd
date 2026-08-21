327dacd4566dec57ea8dfea8abc05892d7c54f62
premasanub <premakumaran15@gmail.com>
2026-08-21 12:55:53 +0530

    updated

--- src/Pages/ForgotPassword.jsx
+++ src/Pages/ForgotPassword.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import api from "../Service/api";
+import api from "../Service/api";   //updated
 
 const ForgotPassword = () => {
   const navigate = useNavigate();
