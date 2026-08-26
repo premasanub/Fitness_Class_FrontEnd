5636d801efea5ec69efc17b5d31d43362f5fa5a9
premasanub <premakumaran15@gmail.com>
2026-08-26 16:27:49 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ import AdminUsers from "./Pages/adminPerson/AdminUsers";
 import AdminTrainers from "./Pages/adminPerson/AdminTrainers";
 import AdminClasses from "./Pages/adminPerson/AdminClasses";
 import AdminBookings from "./Pages/adminPerson/AdminBookings";
-import ReferralOfferManagement from "./Pages/Admin/ReferralOfferManagement";
+import ReferralOfferManagement from "./Pages/adminPerson/ReferralOfferManagement";
 import ReferralOffer from './Pages/userDashboard/ReferralOffer';
 const App = () => {
   return (
