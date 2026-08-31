6ef069a5ef1cd0cfd804c40f1e7797c2389904e7
premasanub <premakumaran15@gmail.com>
2026-08-31 21:01:55 +0530

    home page updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,27 +1,23 @@
-// import Navbar from "../components/Navbar";
+
 import Hero from "../Components/Hero";
-import PopularClasses from   "../Components/PopularClasses";
+import PopularClasses from "../Components/PopularClasses";
 import TopTrainers from "../Components/TopTrainers";
 import WhyChooseUs from "../Components/WhyChooseUs";
 import StatsCounter from "../Components/StatsCounter";
 import Testimonials from "../Components/Testimonials";
 import Pricing from "../Components/Pricing";
-// import Footer from "../Components/Footer";
 
 function Home() {
   return (
-    <>
-      
+    <div className="m-0 p-0">
       <Hero />
       <PopularClasses />
       <TopTrainers />
-       <WhyChooseUs />
-     <StatsCounter />
-     <Testimonials />
-     <Pricing />
-    
-
-    </>
+      <WhyChooseUs />
+      <StatsCounter />
+      <Testimonials />
+      <Pricing />
+    </div>
   );
 }
 
