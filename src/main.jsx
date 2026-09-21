d00b24a37b1ac4234228882cc945d911dc4242f5
premasanub <premakumaran15@gmail.com>
2026-09-21 16:27:52 +0530

    updatede

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import {AuthProvider} from "./Context/AuthContext"
+import App from './App.jsx';
+import {AuthProvider} from "./Context/AuthContext.jsx";
 
 createRoot(document.getElementById('root')).render(
    <AuthProvider>
